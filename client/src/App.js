a8ee6cbf687ef1b2bb3de0bca3a7ecedbed84488
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-06-20 13:37:25 -0600

    adding folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Hello World
-   
-      </header>
+    <div>
+
     </div>
   );
 }
