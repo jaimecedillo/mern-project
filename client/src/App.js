ea655a0a94105bad1ca2bed458058601afb0b8b9
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-06-22 21:05:19 -0600

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar"
 import './App.css';
 
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Signup />
     </div>
   );
