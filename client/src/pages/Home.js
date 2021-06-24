5fdd4e436e6764d3c11ac63fee9a7ae00ff3397e
Nate Hogan <natek.hogan@gmail.com>
2021-06-23 20:41:36 -0600

    product menu

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Grid, TextField, Button, InputAdornment } from '@material-ui/core';
-import { EmailRounded, LockRounded } from "@material-ui/icons"
+import { EmailRounded, LockRounded } from "@material-ui/icons";
+import ProductMenu from "../components/CategoryMenu";
 
 const Home = () => {
     return (
         <div className="container">
-            
+            <Grid container spacing={8}>
+                <Grid className="ProdectMenu">
+                    <ProductMenu />
+                </Grid>
+            </Grid>
         </div>
     );
 };
