9a28908951e2279834a6ebb59588951c626a2da2
Nate Hogan <natek.hogan@gmail.com>
2021-06-27 13:47:07 -0600

    tests

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -9,4 +9,15 @@ export const LOGIN = gql`
             }
         }
     }
-`
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
