91b7f446baec6281ca6560b129620adc984f7a8d
cory alport <cralport@gmail.com>
2021-06-30 19:48:47 -0600

    added to resolvers/typeDefs pages

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,42 @@
-const {User} = require("../models")
-const {signToken,authMiddleware} = require ("../utils/auth")
-const { AuthenticationError} = require("apollo-server-express")
+const { User, Category, Contractor } = require("../models")
+const { signToken, authMiddleware } = require("../utils/auth")
+const { AuthenticationError } = require("apollo-server-express")
 const resolvers = {
-    Query: { 
-        categories: 
-    }, 
-    Mutation:{
-     addUser: async(parent,args) => {
-         const user = await User.create (args)
-        const token = signToken(user)
-        return { user,token }
-     },   
-     login: async(parent,args) => {
-         const user = await User.findOne ({email:args.email})
-         if (!user) {
-             throw new AuthenticationError("could not find user")
-         }
-         const correctPassword = await user.isCorrectPassword (args.password)
-         if (!correctPassword) {
-             throw new AuthenticationError("could not find password")
-         }
-         const token = signToken(user)
-         return { user, token}
-     }
+    Query: {
+        categories: async () => {
+            return await Category.find();
+        },
+        contractors: async () => {
+            return await Contractor.find();
+        },
+        user: async () => {
+            return await User.findById();
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args)
+            const token = signToken(user)
+            return { user, token }
+        },
+        login: async (parent, args) => {
+            const user = await User.findOne({ email: args.email })
+            if (!user) {
+                throw new AuthenticationError("could not find user")
+            }
+            const correctPassword = await user.isCorrectPassword(args.password)
+            if (!correctPassword) {
+                throw new AuthenticationError("could not find password")
+            }
+            const token = signToken(user)
+            return { user, token }
+        },
+        addContractor: async (parent, args) => {
+            const contractor = await Contractor.create(args)
+            const token = signToken(contractor)
+            return { user, token }
+        },
     }
 }
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const { gql } = require ("apollo-server-express")
+const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
 type User { 
@@ -9,7 +9,19 @@ type User {
     password: String
 }
 
+type Contractor {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    workType: String
+}
 
+type Category {
+    _id: ID
+    name: String
+}
 
 type Auth {
     token: ID
@@ -18,15 +30,17 @@ type Auth {
 
 type Query {
     user: User
-   
+    contractor: Contractor
+    categories: [Category]
 }
-
 
 type Mutation {
     addUser(firstName:String!, lastName:String!, email:String!, password:String!):Auth
     login(email:String!, password:String!):Auth
+    updateUser(firstName: String!, lastName: String!, email: String!, password: String!): User
+    addContractor(firstName: String!, lastName: String!, email: String!, password: String!, workType: String!): Auth
+    updateContractor(firstName: String!, lastName: String!, email: String!, password: String!, worktype: String!): Contractor
 }
-
 
 `
 // categories: [ Category ]
