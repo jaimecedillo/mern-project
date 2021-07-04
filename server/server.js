d79e53ca997bcbe2c4e4bfbb9448e630025b1b98
cory alport <cralport@gmail.com>
2021-07-04 01:21:50 -0600

    added readme and made some changes to the server side

--- server/server.js
+++ server/server.js
@@ -5,13 +5,15 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
