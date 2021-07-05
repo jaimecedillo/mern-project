6d057f04e78374342f934a6dd44252602ffe0e15
Nate Hogan <75545915+NathanHogan16@users.noreply.github.com>
2021-07-04 22:58:42 -0600

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Nate-admin:Noottoots1@cluster0.cashk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Nate-admin:yourpassword@cluster0.cashk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
