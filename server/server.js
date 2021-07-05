79fe589ed6bfac38b35a7635f4b374dab2271c3a
Nate Hogan <natek.hogan@gmail.com>
2021-07-04 22:36:43 -0600

    tests

--- server/server.js
+++ server/server.js
@@ -22,6 +22,10 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
