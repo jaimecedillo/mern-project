c55277ff3040cb73a95282906c8bf38bfc0baf12
Nate Hogan <natek.hogan@gmail.com>
2021-07-04 22:43:00 -0600

    tests

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
